61a45f2f3027ea2b8ce338b4dcaab3eef4aec9f8
aplesner <27522600+aplesner@users.noreply.github.com>
2025-04-11 15:07:43 +0200

    Using torch for the code

--- scipy_implementation.py
+++ scipy_implementation.py
@@ -2,6 +2,7 @@ import time
 
 import numpy as np
 from tqdm import tqdm
+import torch
 
 import line_profiler
 
@@ -29,7 +30,7 @@ def _sample_uniform_direction(dim, size, random_state):
     return samples
 
 @line_profiler.profile
-def _rotate_samples(samples, mu, dim):
+def _rotate_samples_torch(samples, mu, dim):
     """A QR decomposition is used to find the rotation that maps the
     north pole (1, 0,...,0) to the vector mu. This rotation is then
     applied to all samples.
@@ -45,18 +46,72 @@ def _rotate_samples(samples, mu, dim):
     samples : rotated samples
 
     """
+    print("Torch implementation")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    samples = torch.tensor(samples, device=device, dtype=torch.float32)
+    mu = torch.tensor(mu, device=device, dtype=torch.float32)
+    with torch.no_grad():
+        base_point = torch.zeros((dim, ), device=device, dtype=torch.float32)
+        base_point[0] = 1.
+        embedded = torch.cat([mu[None, :], torch.zeros((dim - 1, dim), device=device, dtype=torch.float32)], dim=0)
+        embedded = torch.transpose(embedded, 0, 1)
+        start_time = time.time()
+        rotmatrix, _ = torch.linalg.qr(embedded)
+        print(f"Compute rotation time: {time.time()-start_time:.2f}s")
+        # check if the rotation is correct
+        rotated_base_point = torch.mv(rotmatrix, base_point)
+        if torch.allclose(rotated_base_point, mu):
+            rotsign = 1
+        else:
+            rotsign = -1
+        # apply rotation (this is a matrix-vector multiplication)
+
+        # shapes:
+        # samples: (n, dim)
+        # rotmatrix: (dim, dim)
+
+        start_time = time.time()
+        samples2 = torch.matmul(rotmatrix, samples.T).T * rotsign
+        print(f"Apply rotation time: {time.time()-start_time:.2f}s")
+
+        return samples2.cpu().numpy()
+
+
+def _rotate_samples_numpy(samples, mu, dim):
+    """A QR decomposition is used to find the rotation that maps the
+    north pole (1, 0,...,0) to the vector mu. This rotation is then
+    applied to all samples.
+
+    Parameters
+    ----------
+    samples: array_like, shape = [..., n]
+    mu : array-like, shape=[n, ]
+        Point to parametrise the rotation.
+
+    Returns
+    -------
+    samples : rotated samples
+
+    """    
+    print("NumPy implementation")
     base_point = np.zeros((dim, ))
     base_point[0] = 1.
     embedded = np.concatenate([mu[None, :], np.zeros((dim - 1, dim))])
+    start_time = time.time()
     rotmatrix, _ = np.linalg.qr(np.transpose(embedded)) # This line is responsible for 56% of the time
+    print(f"Compute rotation time: {time.time()-start_time:.2f}s")
     if np.allclose(np.matmul(rotmatrix, base_point[:, None])[:, 0], mu):
         rotsign = 1
     else:
         rotsign = -1
 
     # apply rotation (this is a matrix multiplication)
-    samples2 = np.matvec(rotmatrix, samples) * rotsign # This line is responsible for 43% of the time
+    start_time = time.time()
+    samples2 = np.matmul(rotmatrix, samples.T).T * rotsign # This line is responsible for 43% of the time
+    # samples2 = np.matvec(rotmatrix, samples) * rotsign # This line is responsible for 43% of the time
+    print(f"Apply rotation time: {time.time()-start_time:.2f}s")
     return samples2
+
 
 # @line_profiler.profile
 def sample_vMF(mu: np.array, kappa: float, n: int) -> np.ndarray:
@@ -149,30 +204,28 @@ def sample_vMF(mu: np.array, kappa: float, n: int) -> np.ndarray:
     else:
         samples = np.squeeze(samples)
 
-    samples = _rotate_samples(samples, mu, dim) # This line is responsible for 81% of the time
-    return samples
+    # samples_numpy = _rotate_samples_numpy(samples, mu, dim) # This line is responsible for 81% of the time
+    samples_torch = _rotate_samples_torch(samples, mu, dim) # This line is responsible for 81% of the time
+    # check if the rotation is correct
+    # assert np.allclose(samples_numpy, samples_torch), "Rotation is not correct"
+    # # check if the samples are on the sphere
+    # assert np.allclose(np.linalg.norm(samples_numpy, axis=-1), 1), "Samples are not on the sphere"
+    # # check if the samples are on the sphere
+    # assert np.allclose(np.linalg.norm(samples_torch, axis=-1), 1), "Samples are not on the sphere"
+    return samples_torch
 
 def profile_implementation():
     """
     Profile the implementation of the von Mises-Fisher sampling methods.
     """
 
-    mu = np.zeros(1500)
+    mu = np.zeros(3500)
     mu[-1] = 1.0  # Mean direction (unit vector)
     kappa = 10.0  # Concentration parameter
-    n = 15000  # Number of samples
-    # pr = cProfile.Profile(subcalls=False, builtins=False)
-    # pr.enable()
+    n = 5000  # Number of samples
     
     sample_vMF(mu, kappa, n)
 
-    # pr.disable()
-    # pr.dump_stats('restats')
-
-    # # Print the profiling results
-    # p = pstats.Stats('restats')
-    # p.strip_dirs().sort_stats(SortKey.LINE).print_stats()
-    # # Profile the scipy implementation
     
 if __name__ == "__main__":
     profile_implementation()
@@ -181,3 +234,6 @@ if __name__ == "__main__":
 # 2500 from 1500: 1.67x
 # 576765.3 from 200061.2: 2.88x
 # 1724671.8 from 114622.8: 15.05x
+
+# 671613.5
+# 1047640.9 from 
